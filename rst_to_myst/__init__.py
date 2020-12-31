1dd1226e874023ea745f28eeffd50b7a272abba0
Chris Sewell <chrisj_sewell@hotmail.com>
2020-12-31 00:34:38 +0000

    🚀 RELEASE: v0.1.0

--- rst_to_myst/__init__.py
+++ rst_to_myst/__init__.py
@@ -3,4 +3,4 @@ from .namespace import compile_namespace  # noqa: F401
 from .parser import to_ast  # noqa: F401
 from .renderer import convert, render  # noqa: F401
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
