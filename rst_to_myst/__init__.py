edc84bdd9d21f00f04f6fba3f29814b88d6ac7cd
Chris Sewell <chrisj_sewell@hotmail.com>
2021-06-25 08:18:39 +0200

    🐛 FIX: directives in nested states (#19)

--- rst_to_myst/__init__.py
+++ rst_to_myst/__init__.py
@@ -3,4 +3,4 @@ from .mdformat_render import rst_to_myst  # noqa: F401
 from .namespace import compile_namespace  # noqa: F401
 from .parser import to_docutils_ast  # noqa: F401
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
