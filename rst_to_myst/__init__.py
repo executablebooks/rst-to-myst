eed8300c091d36defc36f58c1837c11ab2654854
Chris Sewell <chrisj_sewell@hotmail.com>
2023-01-17 00:25:23 +0100

    🚀 RELEASE: v0.3.4 (#61)

--- rst_to_myst/__init__.py
+++ rst_to_myst/__init__.py
@@ -3,4 +3,4 @@ from .mdformat_render import rst_to_myst  # noqa: F401
 from .namespace import compile_namespace  # noqa: F401
 from .parser import to_docutils_ast  # noqa: F401
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
