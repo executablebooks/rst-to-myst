4f8cc61d7165846dc953a1ae6d05a81762ac9f8f
Chris Sewell <chrisj_sewell@hotmail.com>
2023-07-24 20:02:42 +0200

    🚀 RELEASE: v0.4.0 (#74)

--- rst_to_myst/__init__.py
+++ rst_to_myst/__init__.py
@@ -1,6 +1,8 @@
 """Convert RST to MyST-Markdown."""
-from .mdformat_render import rst_to_myst  # noqa: F401
-from .namespace import compile_namespace  # noqa: F401
-from .parser import to_docutils_ast  # noqa: F401
+from .mdformat_render import rst_to_myst
+from .namespace import compile_namespace
+from .parser import to_docutils_ast
 
-__version__ = "0.3.4"
+__all__ = ("rst_to_myst", "to_docutils_ast", "compile_namespace")
+
+__version__ = "0.4.0"
